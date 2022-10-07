9db10d6f4b8de561398ce03920605b4d690d19ce
Peter Gill <peter.gill@townsuite.com>
2022-10-07 09:54:06 -0230

    add custom nuget package using Baseclass.Contrib.Nuget.Output
    bump version

---
+++ nugetspec/create_nuget.ps1
@@ -0,0 +1,21 @@
+
+
+# See https://github.com/baseclass/Contrib.Nuget, nuget by default does not let output content 
+
+
+function delete_files([string]$path) {
+	If (Test-Path $path) {
+		Write-Host "Deleting path $path" -ForegroundColor Green
+		Remove-Item -recurse -force $path
+	}
+}
+
+delete_files "TownSuite.TwainScanner\output"
+delete_files "NugetBuild"
+mkdir -force TownSuite.TwainScanner\output
+mkdir -force NugetBuild
+
+Copy-Item -force "..\TownSuite.TwainScanner\bin\x86\Release\net462\TownSuite.TwainScanner.exe" TownSuite.TwainScanner\output\TownSuite.TwainScanner.exe
+
+nuget pack TownSuite.TwainScanner\TownSuite.TwainScanner.nuspec -OutputDirectory .\NugetBuild
+
