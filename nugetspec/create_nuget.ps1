c0e6b6bf849855be42859b9576d1a1a2352ebddd
Peter Gill <peter@majorsilence.com>
2024-11-03 19:17:55 -0330

    Update create_nuget.ps1

--- nugetspec/create_nuget.ps1
+++ nugetspec/create_nuget.ps1
@@ -12,10 +12,10 @@ function delete_files([string]$path) {
 
 delete_files "TownSuite.TwainScanner\content"
 delete_files "NugetBuild"
-mkdir -force TownSuite.TwainScanner\content
+mkdir -force TownSuite.TwainScanner\content\TownSuite.TwainScanner
 mkdir -force NugetBuild
 
-Copy-Item -force "..\TownSuite.TwainScanner\bin\x86\Release\net48\TownSuite.TwainScanner.exe" TownSuite.TwainScanner\content\TownSuite.TwainScanner.exe
+Copy-Item -force "..\TownSuite.TwainScanner\bin\x86\Release\net48\TownSuite.TwainScanner.exe" TownSuite.TwainScanner\content\TownSuite.TwainScanner\TownSuite.TwainScanner.exe
 
 nuget pack TownSuite.TwainScanner\TownSuite.TwainScanner.nuspec -OutputDirectory .\NugetBuild
 
